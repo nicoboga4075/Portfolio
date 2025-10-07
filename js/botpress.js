e8357d6b3c7f32039b9eb091c3e6b0cda04f82fa
nicoboga4075 <nicolas.bogalheiro@gmail.com>
2025-10-07 13:31:01 +0200

    Add Chatbot

---
+++ js/botpress.js
@@ -0,0 +1,31 @@
+ if (window.innerWidth >= 1024) {
+	window.botpress.init({
+	  "botId": "ae1c6be3-242d-4559-bda8-8da1cd9b19fc",
+	  "configuration": {
+		"version": "v2",
+		"botName": "Netlify Bot",
+		"botAvatar": "https://files.bpcontent.cloud/2025/10/06/10/20251006102939-SK68F9QM.png",
+		"botDescription": "",
+		"website": {},
+		"email": {},
+		"phone": {},
+		"termsOfService": {},
+		"privacyPolicy": {},
+		"color": "#3B6FFE",
+		"variant": "solid",
+		"headerVariant": "glass",
+		"themeMode": "light",
+		"fontFamily": "inter",
+		"radius": 2.5,
+		"feedbackEnabled": false,
+		"footer": "[⚡ by Botpress](https://botpress.com/?from=webchat)",
+		"soundEnabled": false,
+		"proactiveMessageEnabled": true,
+		"proactiveBubbleMessage": "Hi! 👋 Need help?",
+		"proactiveBubbleTriggerType": "afterDelay",
+		"proactiveBubbleDelayTime": 10
+	  },
+	  "clientId": "ec2878b6-e7e6-49ac-9f2b-de35edd6a080",
+	  "selector": "#webchat"
+	});
+}
